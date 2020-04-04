d781df053336cdc9e8fc6674cefb77099e6fdd6f
Andrew Villarrubia <andrew@primefour.com>
2020-04-04 16:45:02 -0500

    Fuck Heroku.

--- angelarium.js
+++ angelarium.js
@@ -15,14 +15,21 @@ client.on('ready', () => {
 
 client.on('message', msg => {
   if (msg.content === '!invoke') {
-    angel = angels[Math.floor(Math.random() * angels.length)];
-    var attachment = new MessageAttachment(angel.image);
-    try{
-      msg.channel.send(`${msg.member} invoked ${angel.name} from the Angelarium - They are ${angel.description}`, attachment);  
-      console.log(`Success on ${angel.name}`);
+    try {
+      var rand = Math.floor(Math.random() * angels.length)
+      console.log(`Got ${rand} for random array access`);
+      angel = angels[rand];  
+      var attachment = new MessageAttachment(angel.image);
+
+      try {
+        msg.channel.send(`${msg.member} invoked ${angel.name} from the Angelarium - They are ${angel.description}`, attachment);  
+        console.log(`Success on ${angel.name}`);
+      } catch (err) {
+        console.log(err);
+      } 
     } catch (err) {
       console.log(err);
-    } 
+    }
   }
 });
 
